28347ad399a8223a53d68b58590678a6fc11b4f3
Eduardo Palmieri <147361773+eduardopfbp@users.noreply.github.com>
2024-01-14 23:15:00 -0300

    Update and rename principal.py to maino.py

---
+++ maino.py
@@ -0,0 +1,14 @@
+from def_etl import extract, load, concatenar
+
+# STEP 1 - EXTRACT THE DATA
+url_nba = "https://www.basketball-reference.com/friv/dailyleaders.fcgi?month=01&day=13&year=2024&type=all"
+df_temp = extract(url_nba)
+
+# STEP 2 - LOAD THE FILES
+df_temp = load("C://Users//eduar//Desktop//NBA//db//database_temp.xlsx")
+database = load("C://Users//eduar//Desktop//NBA//db//database.xlsx")
+
+# STEP 3 - CONCATENATE THE FILES
+data = concatenate(database, df_temp)
+
+print(data)
